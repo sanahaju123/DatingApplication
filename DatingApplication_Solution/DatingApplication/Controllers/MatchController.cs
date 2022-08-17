1a2a38d5b46b35be624cbc6a1d4e5409bc684815
sanahaju123 <88725494+sanahaju123@users.noreply.github.com>
2022-08-17 08:42:05 +0530

    Updated

--- DatingApplication_Solution/DatingApplication/Controllers/MatchController.cs
+++ DatingApplication_Solution/DatingApplication/Controllers/MatchController.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 
 namespace DatingApplication.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class MatchController : ControllerBase
     {
